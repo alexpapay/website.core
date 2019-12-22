e457d7f084d25bd4b62b593ad72a58915e183050
alexpapay <alexpapay@mail.ru>
2019-12-22 22:29:29 +0100

    Added data access layer.

---
+++ website.dal/Auth/AuthRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using website.core.Models.Auth;
+using website.dal.Interfaces;
+using website.data;
+
+namespace website.dal.Auth
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly IdentityContext _context;
+
+        public AuthRepository(IdentityContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Get all identity users from database.
+        /// </summary>
+        /// <returns>List of identity users.</returns>
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+    }
+}
---
+++ website.dal/Interfaces/IAuthRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using website.core.Models.Auth;
+
+namespace website.dal.Interfaces
+{
+    public interface IAuthRepository
+    {
+        /// <summary>
+        /// Get all identity users from database.
+        /// </summary>
+        /// <returns>List of identity users.</returns>
+        Task<List<User>> GetAllUsers();
+    }
+}
