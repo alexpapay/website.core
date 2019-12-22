177cbd74917c59f6cbf45b5930423a5243728a8c
alexpapay <alexpapay@mail.ru>
2019-12-22 22:28:17 +0100

    Added packages to core project.

---
+++ website.core/Models/Auth/User.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace website.core.Models.Auth
+{
+    /// <summary>
+    /// ASP Identity User model class for creation EF migration and potential properties extension.
+    /// </summary>
+    public class User : IdentityUser
+    {
+        // Add any property here. For example, Date of birth or City.
+    }
+}
